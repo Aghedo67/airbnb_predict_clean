7c9b9c3f0af9513087721a1b35d374ee00b774c9
Aghedo67 <113692288+Aghedo67@users.noreply.github.com>
2026-04-15 19:56:04 +0100

    Update suggestion.py

--- Recommendation_system/suggestion.py
+++ Recommendation_system/suggestion.py
@@ -41,7 +41,7 @@ def load_data():
 
 @st.cache_resource
 def load_suggestion_model():
-    return joblib.load('recommendation_system/baseline_model.pkl')
+    return joblib.load('Recommendation_system/baseline_model.pkl')
 
 def get_image(url):
     try:
